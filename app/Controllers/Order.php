96d27d5e1d7045af84ca9a127e9663a588699fba
AufarR <aufaralazhar@gmail.com>
2023-12-16 05:54:21 +0700

    Update order API

--- app/Controllers/Order.php
+++ app/Controllers/Order.php
@@ -1,14 +1,34 @@
 <?php
 namespace App\Controllers;
 use CodeIgniter\RESTful\ResourceController;
+use App\Models\Mobil;
 use App\Models\Assembly;
 
 class Order extends ResourceController{
     public function create(){
         if (trim($this->request->header('Authorization')) !== trim("Authorization: Bearer " . getenv('api.key')))
             return $this->response->setStatusCode(401)->setHeader('WWW-Authenticate','Bearer');
-        $model = model(Assembly::class);
+
+        $mobil = model(Mobil::class);
+        $assembly = model(Assembly::class);
+        date_default_timezone_set("Asia/Jakarta");
+
         $mobilId = $this->request->getJsonVar('mobilId');
-        return $this->response->setStatusCode(201)->setJSON($model->order($mobilId));
+        $jumlah = $this->request->getJsonVar('jumlah');
+
+        $readyStock = $mobil->order($mobilId,$jumlah);
+        if ($readyStock < $jumlah) {
+            $backOrder = $assembly->order($mobilId,$jumlah-$readyStock);
+            $returnObject = [
+                'backOrder' => true,
+                'waitTime' => max((int) date_diff(date_create('yesterday'),date_create($backOrder['tglSelesai']))->format("%r%d"),0)
+            ];
+        } else {
+            $returnObject = [
+                'backOrder' => false,
+            ];
+        }
+
+        return $this->response->setStatusCode(201)->setJSON($returnObject);
     }
 }
--- app/Models/Mobil.php
+++ app/Models/Mobil.php
@@ -2,7 +2,21 @@
 namespace App\Models;
 use CodeIgniter\Model; class Mobil extends Model{
     protected $table = 'mobil';
+    protected $allowedFields = ['stok'];
+
     public function getDataMobil(){
         return $this->findAll();
+    }
+    public function order($mobilId, $qty) {
+
+        $oldStok = $this->find($mobilId)['stok'];
+        $newStok = max($oldStok-$qty,0);
+
+        $data = [
+            'stok' => $newStok
+        ];
+        $this->update($mobilId,$data);
+
+        return $oldStok-$newStok;
     }
 }
