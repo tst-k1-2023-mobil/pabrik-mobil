a11bf006cf97a6bc0b80019144eb25d5df1a9bb9
AufarR <aufaralazhar@gmail.com>
2023-12-15 17:02:53 +0700

    Add api key auth

--- app/Controllers/Order.php
+++ app/Controllers/Order.php
@@ -5,6 +5,8 @@ use App\Models\Assembly;
 
 class Order extends ResourceController{
     public function create(){
+        if (trim($this->request->header('Authorization')) !== trim("Authorization: Bearer " . getenv('api.key')))
+            return $this->response->setStatusCode(401)->setHeader('WWW-Authenticate','Bearer');
         $model = model(Assembly::class);
         $mobilId = $this->request->getJsonVar('mobilId');
         return $this->response->setStatusCode(201)->setJSON($model->order($mobilId));
--- app/Controllers/Queue.php
+++ app/Controllers/Queue.php
@@ -5,6 +5,8 @@ use App\Models\Assembly;
 
 class Queue extends ResourceController{
     public function index(){
+        if (trim($this->request->header('Authorization')) !== trim("Authorization: Bearer " . getenv('api.key')))
+            return $this->response->setStatusCode(401)->setHeader('WWW-Authenticate','Bearer');
         $model = model(Assembly::class);
         $data = ['queueLength' => max((int) date_diff(date_create('yesterday'),date_create($model->getAssemblyQueue()))->format("%r%d"),0)];
         return $this->response->setStatusCode(200)->setJSON($data);
