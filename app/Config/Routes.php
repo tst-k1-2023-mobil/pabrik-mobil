c7142d257c2432560fcd4bfdf6d6b267ca3b5722
WillyFFS <willyfrans13@gmail.com>
2023-12-15 16:55:08 +0700

    nambahin feat listmobil

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,3 +13,4 @@ $routes->get('/api/queue', 'Queue::index');
 $routes->post('/api/order', 'Order::create');
 $routes->get('/admin', 'Admin::index');
 $routes->post('/admin', 'Admin::index');
+$routes->get('/listmobil', 'ListMobil::index');
